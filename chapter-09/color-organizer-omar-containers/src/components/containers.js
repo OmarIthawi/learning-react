5dd6e8a5dde2a70e294aa18ff65890ae2bd800bd
Omar Al-Ithawi <i@omardo.com>
2020-09-02 12:09:09 +0300

    rr connect!

--- chapter-09/color-organizer-omar-containers/src/components/containers.js
+++ chapter-09/color-organizer-omar-containers/src/components/containers.js
@@ -6,40 +6,39 @@ import SortMenu from './SortMenu'
 import { sortFunction } from '../lib/array-helpers'
 
 import { addColor, sortColors, rateColor, removeColor } from '../actions'
+import { connect } from 'react-redux'
 
 
-export const NewColor = (props, { store }) =>
-    <AddColorForm 
-        onNewColor={(title, color) => store.dispatch( addColor(title, color) )}
-        />
+export const NewColor = connect(
+    () => ({}),
+    dispatch => ({
+        onNewColor(title, color) {
+            dispatch(addColor(title, color))
+        }
+    })
+)(AddColorForm)
 
-NewColor.contextTypes = {
-    store: PropTypes.object
-}
+export const Menu = connect(
+    state => ({
+        sort: state.sort
+    }),
+    dispatch => ({
+        onSelect(sortBy) {
+            dispatch(sortColors(sortBy))
+        }
+    })
+)(SortMenu)
 
-export const Menu = (props, { store }) =>
-    <SortMenu 
-        sort={ store.getState().sort }
-        onSelect={sortBy => store.dispatch( sortColors(sortBy) )}
-        />
-
-Menu.contextTypes = {
-    store: PropTypes.object
-}
-
-export const Colors = (props, { store }) => {
-    const { colors, sort } = store.getState()
-    const sortedColors = [...colors].sort(sortFunction(sort))
-
-    return (
-        <ColorList 
-            colors={sortedColors}
-            onRemove={id => store.dispatch( removeColor(id) )}
-            onRate={(id, rating) => store.dispatch( rateColor(id, rating) )}
-            /> 
-    )
-}
-    
-Colors.contextTypes = {
-    store: PropTypes.object
-}
+export const Colors = connect(
+    state => ({
+        colors: [...state.colors].sort(sortFunction(state.sort))
+    }),
+    dispatch => ({
+        onRemove(id) {
+            dispatch(removeColor(id))
+        },
+        onRate(id, rating) {
+            dispatch(rateColor(id, rating))
+        }
+    })
+)(ColorList)
