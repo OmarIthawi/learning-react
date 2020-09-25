8d18be91e718a15176a0c3a3d428aea25ffd9e17
Omar Al-Ithawi <i@omardo.com>
2020-09-25 12:23:58 +0300

    click fails

--- chapter-10/omar-color-organizer/__tests__/star.test.js
+++ chapter-10/omar-color-organizer/__tests__/star.test.js
@@ -2,5 +2,21 @@ import { shallow } from 'enzyme'
 import Star from '../src/components/ui/Star'
 
 describe('<Star /> component', () => {
-    it("render default star", () => expect(1).toBe(1))
+    it("render default star", () => expect(
+        shallow( <Star /> ).find('div.star').length
+    ).toBe(1))
+
+    it("render selected star", () => expect(
+        shallow( <Star selected={true} /> ).find('div.star.selected').length
+    ).toBe(1))
+
+    // it('invokes onClick', () => {
+    //     const _click = jest.fn()
+    //     expect(_click).toBeCalled()
+    //
+    //     shallow(<Star onClick={_click} />)
+    //         .find('dev.star')
+    //         .simulate('click')
+    //
+    // })
 })
